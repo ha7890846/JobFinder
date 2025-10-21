92bec70552dd7445e92db0346e52b57ec01b9acb
ha7890846 <ha7890846@gmail.com>
2025-10-21 10:32:43 +0530

    added navigation to job

--- src/FindJobs/JobCard.tsx
+++ src/FindJobs/JobCard.tsx
@@ -1,9 +1,10 @@
 import { IconBookmark, IconClockHour3 } from "@tabler/icons-react";
 import {Divider, Text} from "@mantine/core";
+import { Link } from "react-router-dom";
 
 const JobCard=(props:any)=>{
     return (
-        <div className="bg-mine-shaft-900 p-5 w-80 flex flex-col gap-2 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400 transition duration-300 ease-in-out ">
+        <Link to="/jobs" className="bg-mine-shaft-900 p-5 w-80 flex flex-col gap-2 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400 transition duration-300 ease-in-out ">
             <div className="flex justify-between">
                 <div className="flex gap-2 items-center">
                     <div className="p-2 bg-mine-shaft-800 rounded-md">
@@ -32,7 +33,7 @@ const JobCard=(props:any)=>{
                     <IconClockHour3 className="h-5 w-5" stroke={1.5}/> {props.postedDaysAgo} days Ago
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 export default JobCard;
